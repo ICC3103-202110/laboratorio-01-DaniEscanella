595fe3d5f6c0814918a1e6968ef6431be79c48d9
Daniela Escanella <dani@MacBook-Pro-de-Daniela-94.local>
2021-03-23 19:02:35 -0300

    guardado

--- Lab_1.py
+++ Lab_1.py
@@ -1,1 +1,22 @@
-print("hi")
+def imprimir_mesa(largo,ancho):
+    for i in range(largo+1):
+        fila1="   "
+        fila2=" "
+        for k in range(ancho):
+            fila1+=str(k)+" "
+            if i!=0:
+                fila2+="* "
+        if i==0:
+            print(fila1)
+            
+        elif i>0 and i<11:
+            print(" "+str(i-1)+fila2)
+        else:
+            print(str(i-1)+fila2)
+
+
+
+mazo=int(input("Inserte mazo:"))
+cartas=mazo*2
+
+imprimir_mesa(20,10)
